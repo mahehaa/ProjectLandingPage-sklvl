18034fbf28dfc85d16fa0aeb3f62cdb896d9c14f
Mahesa Alamsyah <mahesanuralamsyah@gmail.com>
2023-03-25 02:39:40 +0700

    commit mahe3

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 let cardList = [
    {
-    namaKelas : "Seni Rupa",
-    deskripsi : "deskripsi"
+      namaKelas : "Seni Rupa",
+      deskripsi : "deskripsi",
+      image : "https://i.postimg.cc/DZyhyMzZ/adrianna-geo-1r-Bg5-YSi00c-unsplash.jpg"
    },
    {
-    namaKelas : "Seni Rupa",
-    deskripsi : "deskripsi"
+      namaKelas : "Seni Rupa",
+      deskripsi : "deskripsi",
+      image : "https://i.postimg.cc/DZyhyMzZ/adrianna-geo-1r-Bg5-YSi00c-unsplash.jpg"
    },
    {
-    namaKelas : "Seni Rupa",
-    deskripsi : "deskripsi"
+      namaKelas : "Seni Rupa",
+      deskripsi : "deskripsi",
+      image : "https://i.postimg.cc/DZyhyMzZ/adrianna-geo-1r-Bg5-YSi00c-unsplash.jpg"
+   },
+   {
+      namaKelas : "Seni Rupa",
+      deskripsi : "deskripsi",
+      image : "https://i.postimg.cc/DZyhyMzZ/adrianna-geo-1r-Bg5-YSi00c-unsplash.jpg"
    },
 ]
+
+function generateCards(data) {
+   let cards = "";
+
+   data.forEach(function (item) {
+       cards += `
+       <div class="card">
+       <img src="${item.image}" alt="seniRupa" style="width:100%">
+          <div class="container-kelas">
+             <h4><b>${item.namaKelas}</b></h4> 
+             <p>${item.deskripsi}</p> 
+          </div>          
+    </div>
+   `;
+   });
+
+   return cards;
+}
+
+let cardContainer = document.getElementById("card-container");
+cardContainer.innerHTML = generateCards(cardList);
