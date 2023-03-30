0eaa1abbdb89d9161041903aceae133079f858a4
Mahesa Alamsyah <mahesanuralamsyah@gmail.com>
2023-03-30 23:22:14 +0700

    edit form

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+// validasi form
+function handleGetFormData() {
+   let obj = {
+       name : document.getElementById("name").value,
+       email : document.getElementById("email").value,
+       city : document.getElementById("city").value,
+       zipCode : document.getElementById("zip-code").value,
+       status : document.getElementById("status").checked
+   }
+   return obj
+}
+
+function isNumber(input) {
+   if (isNaN(input)) { return false } 
+   else { return true }
+}
+
+function checkboxIsChecked() {
+   let isCheck = document.getElementById("status").checked
+   if(isCheck) { return true } 
+   else { return false }
+}
+
+function validateFormData({name, city, email, zipCode, status}) {
+   let obj = {name, city, email, zipCode, status}
+   for (let data in obj) {
+       let checkNumber = isNumber(obj.zipCode)
+       let isChecked = checkboxIsChecked(obj.status)
+       if (obj[data] !== null && checkNumber && isChecked) {
+           return true
+       } else { return false }
+   }
+}
+
+let submitForm = document.getElementById("submit-form")
+submitForm.addEventListener("submit", () => {submit()})
+
+function submit(event) {
+   let dataValidasi = validateFormData(handleGetFormData())
+   if (dataValidasi === false) {
+       return document.getElementById("warning").innerHTML = "Periksa form anda sekali lagi";
+   } else {
+       return document.getElementById("warning").innerHTML = "";
+   }
+   event.preventDefault()
+}
+// validasi form
+
 // Active Menu
 let navbar = document.getElementsByClassName("menu");
 let btns = navbar[0].getElementsByClassName("btn");
